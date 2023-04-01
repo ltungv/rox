50b7391c0cb34d9f82d6d36f2f8913e8a6c60cf7
letung3105 <15053897+letung3105@users.noreply.github.com>
2023-04-01 11:31:29 +0700

    add benchmarks

---
+++ benches/e2e.rs
@@ -0,0 +1,65 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rox::VirtualMachine;
+
+pub fn interpret(c: &mut Criterion) {
+    let mut g = c.benchmark_group("e2e");
+
+    g.bench_function("binary_tree.lox", |b| {
+        let src = include_str!("e2e/binary_tree.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.bench_function("equality.lox", |b| {
+        let src = include_str!("e2e/equality.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.bench_function("fib20.lox", |b| {
+        let src = include_str!("e2e/fib20.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.bench_function("instantiation.lox", |b| {
+        let src = include_str!("e2e/instantiation.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.bench_function("invocation.lox", |b| {
+        let src = include_str!("e2e/invocation.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.bench_function("method_call.lox", |b| {
+        let src = include_str!("e2e/method_call.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.bench_function("properties.lox", |b| {
+        let src = include_str!("e2e/properties.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.bench_function("trees.lox", |b| {
+        let src = include_str!("e2e/trees.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.bench_function("zoo.lox", |b| {
+        let src = include_str!("e2e/zoo.lox");
+        let mut vm = VirtualMachine::default();
+        b.iter(|| vm.interpret(black_box(src)));
+    });
+
+    g.finish();
+}
+
+criterion_group!(e2e, interpret);
+criterion_main!(e2e);
